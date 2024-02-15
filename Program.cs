267361aaee918e81d8d3ae4d552391621505b0da
SANDRO WUTHRICH <aleksi.w@protonmail.com>
2024-02-15 14:29:52 +0200

    programm files added

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Muuttujat mielialan ja kunnon tallentamiseen
+            string vireysStr;
+            int vireys;
+            string uniStr;
+            int uni;
+            string nalkaStr;
+            int nalka;
+            int moodIndex;
+            Console.WriteLine("Hyvää huomenta, miltä sinusta tänään tuntuu?");
+            Console.WriteLine();
+            Console.WriteLine("Arvioi vireyttäsi astekoilla 1-5 1:unelias, 5: täynnä räjähtävää toimintotarmoa");
+            vireysStr = Console.ReadLine();
+            Console.WriteLine("Miten hyvin nukuit? 1: huonosti, 5: kuin tukki");
+            uniStr = Console.ReadLine();
+            Console.WriteLine("Miten nälkäinen olet? 1: kaamea nälkä, 5 ei muruakaan");
+            nalkaStr = Console.ReadLine();
+
+            // Muutetaan vastaukset numeromuotoon
+            vireys = int.Parse(vireysStr);
+            uni = int.Parse(uniStr);
+            nalka = int.Parse(nalkaStr);
+
+            // Lasketaan mielialaindexin
+            moodIndex = vireys + uni + nalka;
+
+            // Tulostetaan käyttäjän antamat vastaukset
+            Console.WriteLine("viereyspisteet: " + vireysStr + ", unipistieet: " + uniStr + " , nälkäpisteet: " + nalkaStr);
+            Console.WriteLine();
+            Console.WriteLine("Mielialaindeksi on " + moodIndex);
+
+
+            //Pidetään konsoli-ikkuna auki, kunnes painetaan <Enter>
+            Console.ReadLine();
+        } 
+    }
+
+}
---
+++ luokka.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luokkaharjoituksia
+{
+    class Hooman
+    {
+        // Define properties of Hooman
+        public string name = "Pat Pattern";
+        public int age = 30;
+        public string gender = "other";
+
+        // Default constructor without argument 
+        // No need to define, will be crated automatically
+        public Hooman()
+        {
+
+        }
+
+        // Consturctor with one argument
+        public Hooman(string name)
+        {
+            this.name = name;
+        }
+
+        // Constructor with two arguments
+        public Hooman(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Constructor with three arguments
+        public Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+        
+        public void SayOpinion()
+        {
+            Console.WriteLine("Voi lemmikit ne on elämän suloa");
+        }
+    }
+
+    class DogOwner : Hooman
+    {
+        public new void SayOpinion()
+        {
+            Console.WriteLine("Koira on uskollinen ja luotettava kumppani, ihmisen paras ystävä");
+        }
+    }
+
+    class Pet
+    {
+        public string name = "Beasty";
+
+        public Pet() { }
+
+        public Pet(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void Eats()
+        {
+            Console.WriteLine(this.name + " eats food.");
+        }
+    }
+
+    class Hare : Pet  
+    {   
+        public Hare() { }
+
+        public Hare(string name)
+        {
+            this.name = name;
+        }
+        public override void Eats()
+        {
+            Console.WriteLine(this.name + " eats carrots");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create (instantiate) a hooman object from Hooman class
+            Hooman owner = new Hooman("Sandro Wüthrich", 45, "mies");
+
+            // Call say Opinion method
+            owner.SayOpinion();
+
+            string who = owner.name;
+
+            Console.WriteLine("totesi " + who);
+
+            DogOwner dogOwner = new DogOwner();
+            dogOwner.SayOpinion();
+
+            Pet myPet = new Pet("Perry");
+            myPet.Eats();
+
+            Hare peeky = new Hare("Bulby");
+            peeky.Eats();
+
+            
+            Console.ReadLine();
+        }
+    }
+
+    
+    
+}
